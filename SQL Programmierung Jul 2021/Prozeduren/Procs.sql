abcb6698eeb0daaea70da9b66b6d0e6312d153e0
Rauchendraser <andreasr@ppedv.de>
2021-07-12 15:12:16 +0200

    Prozeduren, Partition, part Sicht

---
+++ SQL Programmierung Jul 2021/Prozeduren/Procs.sql
@@ -0,0 +1,102 @@
+--Prozeduren..
+
+
+/*
+..sind schneller.. aber warum?
+Mythos: kompiliert.. der Plan wird hinterlegt
+
+
+*/
+
+--Pläne..
+set statistics io , time on
+select * from orders where CustomerID like 'A%'
+
+--SCAN: von A bis Z alles durchsuchen
+--SEEK: gezieltes heraussuchen.. schnellste Suche
+
+--Im Plan sind auch Kosten und Schätzwerte
+--Der Plan errechnet Kosten (SQL Dollar)
+
+--Idee der Proz: der Plan wird einmal errechnet und hinterlegt
+---und ist auch nach dem Neustart immer vorhanden
+
+--Proz funktioniert wie Batch
+create proc gpDemo @par1 int
+as
+select * from tabelle where Sp = @par1
+Go
+
+exec gpDemo 2
+
+--Schreibe eine Prozedur, die nach Kunden sucht
+select * from customers
+
+exec gpKundenSuche 'ALFKI' --1 Kunde
+exec gpKundenSuche 'A%' --4 Kunde
+exec gpKundenSuche '%' --alle Kunden
+
+
+alter proc gpKundenSuche @CustID varchar(5)='%'
+as
+select * from Customers where CustomerID like @Custid +'%'
+
+exec gpKundenSuche 'ALFKI' --1 Kunde
+
+
+
+
+exec gpKundenSuche 'A' --4 Kunde
+exec gpKundenSuche  --alle Kunden
+
+--ist aber schlecht
+
+
+--Vorteil der Prozedur: Plan wir einmal festgelegt
+--mit dem ersten Aufruf (mit dem ersten Parameter)
+
+--der Plan wird nicht mehr geändert.. alsio insofern werden 
+--ungünstige Pläne verwendet
+
+
+--Tipp: Proz drüfen nicht benutzerfreundlich sein
+
+create proc gpkundensuche @par char(5)
+as
+if
+ exec gpKundensuche1 
+else
+exec gpkundensuche2 '%'
+
+
+
+
+
+
+
+
+--
+
+create proc gpdemo2 @netto int, @brutto int output
+as
+select @brutto = @netto * 1.19;
+GO
+
+--der Output hat es auf sich
+
+exec gpdemo2 100 --error erwartet den Brutto Parameter
+
+exec gpdemo2 100,100 --.. wie komm ich an Brutto
+
+declare @ergebnis as int --Variable ausserhalb deklarieren
+exec gpdemo2 100, @brutto=@ergebnis output  --Output damit der Wert in die ext Var zurückgeschrieben wird
+select @ergebnis 
+
+
+declare @ergebnis as int --Variable ausserhalb deklarieren
+exec gpdemo2 100, @ergebnis=@brutto output  --Output damit der Wert in die ext Var zurückgeschrieben wird
+select @ergebnis 
+
+exec gpdemo2 @brutto=3, @netto= 100
+
+
--- SQL Programmierung Jul 2021/Salamitaktik/04_Partitionierung.sql
+++ SQL Programmierung Jul 2021/Salamitaktik/04_Partitionierung.sql
@@ -63,15 +63,124 @@ while @i <=20000
 set statistics io, time on
 
 select * from ptab where nummer = 117 -- Lesevorgänge: 100,
-select * from ptab where id = 117
+select * from ptab where id = 117 --20000
 
 
 --Grenzen sind extrem einfach manipulierbar
 --Archivierung
 
 
+select * from ptab where nummer = 1117  --19800
+
+-----------100------200-------------5000  ----------
+
+--neue Grenze
+--ptab  f()  schZahl
+
+--ptab  egal.. 
+--zuerst das schema
+
+--Dgruppe zu Schema hinzufügen
+alter partition scheme schZahl next used bis5000
+
+
+select $partition.fzahl(nummer),
+	min(nummer), max(nummer), count(*)
+from ptab
+group by $partition.fzahl(nummer)
+
+--neue Grenze 5000
+alter partition function fzahl()
+		split range(5000)
+
+select * from ptab where nummer = 1117 --4800 Seiten
+
+--Tipp: die Partitionen verhalten sich wie Tabellen
+
+----100-------200---------------5000--------------
+
+--Grenze 100 entfernen...
+--ptab  f() schema
+
+--f()  
+
+alter partition function fzahl() merge range(100)
+
+select * from ptab where nummer =117
+
+--------200-------------5000-------------
+
+
+select * from ptab where id = 100
+--Idee : DS die man nicht mehr braucht.. archivieren
+
+--TSQL verschiebe DS von a nach b
+
+/****** Object:  PartitionScheme [schZahl]    Script Date: 12.07.2021 13:56:46 ******/
+CREATE PARTITION SCHEME [schZahl] 
+AS 
+PARTITION [fZahl] TO ([bis200], [bis5000], [rest])
+
+/****** Object:  PartitionFunction [fZahl]    Script Date: 12.07.2021 13:57:31 ******/
+CREATE PARTITION FUNCTION [fZahl](int) AS
+RANGE LEFT FOR VALUES (200, 5000)
+GO
+
+--Archivtabelle
+
+create table archiv --muss auf DGruppen liegen, die später geswitched wird
+		( id int not null
+		, nummer int
+		, spx char(4100)) ON bis200
+
+
+alter table ptab switch partition 1 to archiv--
+
+
+--100 MB/sek
+--10000000000000000 MB Bereich 1 ..2ms
+
+select * from archiv
+
+select * from ptab where nummer = 117
+
+
+--Wieviele Teile kann man machen: 15000 pro Tabelle
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+--Jahresweise
+create partition function fZahl(datetime)
+as
+RANGE LEFT FOR VALUES('31.12.2021 23:59:59.999','')
+
+----abisM   NbisR  SbisZ
+create partition function fZahl(varchar(50))
+as
+RANGE LEFT FOR VALUES('MZZZZZZZ','S')
+
+
+select $partition.fzahl(117) -- 2
+
+
+--Part Schema
+create partition scheme schZahl
+as
+partition fzahl to ([PRIMARY],[PRIMARY],[PRIMARY])
+
+--ist auch gut.. weil sich die kleine Teile wie kleine Tabellen verhalten
+
+-----
